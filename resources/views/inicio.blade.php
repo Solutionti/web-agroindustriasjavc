e0bb36f1890b05e5f530c2b4e27ef71c67e6dc13
Desarrollador Jerson Galvez <desarrollo.gocho@gmail.com>
2022-12-11 20:41:10 -0500

    subiendo la configuracion de la pagina web para iniciar con el diseño

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/asclepio.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>AgroindustriasJAVC</title>
+</head>
+<body>
+<header>
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
+    <div class="container">
+      <a class="navbar-brand" href="#"><strong>MDB</strong></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7"
+        aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">INICIO <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ACERCA DE</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">SERVICIOS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">CONTACTO</a>
+          </li>
+        </ul>
+        <form class="form-inline">
+          <div class="md-form my-0">
+            <button class="btn btn-outline-white text-white">CONOCER MAS</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </nav>
+  <div class="view jarallax" data-jarallax='{"speed": 0.2}' style="background-image: url('https://mdbootstrap.com/img/Photos/Others/img%20%2848%29.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+    <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 mb-4 white-text text-center">
+            <h1 class="h1-reponsive texto-grande white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5 wow fadeInDown"
+              data-wow-delay="0.3s"><strong>AgroindustriasJAVC</strong></h1>
+              <h5 class="text-uppercase mb-5 mt-2 white-text wow fadeInDown" data-wow-delay="0.4s"><strong>EMPRESA AGROPECUARIA</strong></h5>
+              <hr class="hr-light my-4 wow fadeInDown" data-wow-delay="0.4s">
+            <a class="btn btn-outline-white wow fadeInDown" data-wow-delay="0.4s">portfolio</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<br>
+<br>
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-md-4">
+      <h1 class=" font-weight-bold">AgriCom <br> Farm Ecology Products</h1>
+    </div>
+    <div class="col-md-8">
+        <p class="lead">
+          Smells racy free announcing than durable zesty smart exotic far feel. Screamin'
+          affordable secret way absolutely.
+        </p>
+        <p>
+           Evulates vast a real proven works discount secure care. Market invigorate a awesome
+           handcrafted bigger comes newer recommended lifetime. Odor to yummy high racy bonus
+           soaking mouthwatering. First superior full-bodied drink. Like outstanding odor economical
+           deal clinically
+        </p>
+        <div class="row ">
+          <div class="col-md-3 mt-3">
+            <img
+              src="https://img.freepik.com/iconos-gratis/whatsapp_318-162854.jpg?w=2000"
+              class="img-fluid"
+              width="90px;" 
+            />
+          </div>
+          <div class="col-md-3 mt-3">
+            <img
+              src="https://img.freepik.com/iconos-gratis/whatsapp_318-162854.jpg?w=2000"
+              class="img-fluid"
+              width="90px;"
+            />
+          </div>
+          <div class="col-md-3 mt-3">
+            <img
+              src="https://img.freepik.com/iconos-gratis/whatsapp_318-162854.jpg?w=2000"
+              class="img-fluid"
+              width="90px;"
+            />
+          </div>
+          <div class="col-md-3 mt-3">
+            <img
+              src="https://img.freepik.com/iconos-gratis/whatsapp_318-162854.jpg?w=2000"
+              class="img-fluid"
+              width="90px;"
+            />
+          </div>
+        </div>
+    </div>
+  </div>
+</div>
+<!-- <div class="container mt-5">
+    <div class="col-md-6">
+        <p class="text-dark display-1">Awesome Harvest of Products</p>     
+    </div>
+</div> -->
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="container-fluid ">
+        <section class="  text-center text-lg-left dark-grey-text">
+          <div class="row">
+            <div class="col-md-5 mb-4 mb-md-0 offset-md-1">
+              <h1 class="font-weight-bold">La cosecha más grande de la historia</h1>
+                <p class=" lead">
+                  Huele picante, anunciando más que una sensación duradera, sabrosa, elegante y exótica.
+                  Screamin 'manera secreta asequible absolutamente.
+                </p>
+                <p class="text-muted">
+                  Estimula una gran atención segura con descuento de obras reales probadas.
+                   El mercado fortalece un impresionante hecho a mano más grande viene más
+                    nuevo recomendado de por vida.
+                </p>
+                <p class="text-muted">
+                  Estimula una gran atención segura con descuento de obras reales comprobadas.
+                  El mercado fortalece un impresionante hecho a mano más grande viene más nuevo
+                  recomendado de por vida. Evalúa una gran atención segura con descuento de obras
+                  reales probadas. El mercado fortalece un impresionante hecho a mano más grande
+                  viene más nuevo recomendado de por vida. Olor a delicioso bonificación de alta 
+                  picante que hace agua la boca. primer superior
+                </p>
+              <a class="btn btn-outline-info btn-lg ml-0" href="#" role="button">Saber mas</a>
+            </div>
+            <div class="col-md-6 mb-4 mb-md-0">
+              <div class=" overlay z-depth-1-half">
+                <img src="https://mdbootstrap.com/img/Photos/Others/img%20(28).jpg" class="img-fluid"
+                  alt="">
+                <a href="#">
+                  <div class="mask rgba-white-light"></div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <br>
+<br>
+<br>
+<br>
+<br>
+      <!-- FOOTER -->
+      <footer class="page-footer font-small mdb-color pt-4">
+      <div class="container text-center text-md-left">
+        <div class="row text-center text-md-left mt-3 pb-3">
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Company name</h6>
+            <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+              consectetur
+              adipisicing elit.</p>
+          </div>
+          <hr class="w-100 clearfix d-md-none">
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+            <p>
+              <a href="#!">MDBootstrap</a>
+            </p>
+            <p>
+              <a href="#!">MDWordPress</a>
+            </p>
+            <p>
+              <a href="#!">BrandFlow</a>
+            </p>
+            <p>
+              <a href="#!">Bootstrap Angular</a>
+            </p>
+          </div>
+          <hr class="w-100 clearfix d-md-none">
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+            <p>
+              <a href="#!">Your Account</a>
+            </p>
+            <p>
+              <a href="#!">Become an Affiliate</a>
+            </p>
+            <p>
+              <a href="#!">Shipping Rates</a>
+            </p>
+            <p>
+              <a href="#!">Help</a>
+            </p>
+          </div>
+          <hr class="w-100 clearfix d-md-none">
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+            <p>
+              <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+            <p>
+              <i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
+            <p>
+              <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+            <p>
+              <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+          </div>
+        </div>
+        <hr>
+        <div class="row d-flex align-items-center">
+          <div class="col-md-7 col-lg-8">
+            <p class="text-center text-md-left">© 2023 todos los derechos
+              <a href="/">
+                <strong> agroindistriasjavc.com</strong>
+              </a>
+            </p>
+          </div>
+          <div class="col-md-5 col-lg-4 ml-lg-0">
+          </div>
+        </div>
+      </div>
+    </footer>
+<script src="{{ asset('js/asclepio.min.js') }}"></script>
+<script>
+    new WOW().init();
+</script>
+</body>
+</html>
